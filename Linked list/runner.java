5cf1e1286852db53f62fb0fe85c3288fd6a350f9
Shreyangshu <79297450+monku12@users.noreply.github.com>
2021-10-02 21:01:29 +0530

    Add files via upload

---
+++ Linked list/runner.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class runner 
+{
+	public static void main(String[] args) 
+    {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number");
+
+		LinkedList list = new LinkedList();
+
+		list.insert(2);
+		list.insert(5);
+		list.insert(7);
+
+		list.deleteAt(2);
+		
+		list.show();
+	}
+
+} 
