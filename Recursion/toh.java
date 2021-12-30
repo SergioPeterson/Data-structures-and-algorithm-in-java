21a9533841198fc9d0fbd7837fb759a2b3313a1d
Shreyangshu <79297450+monku12@users.noreply.github.com>
2021-12-30 10:31:43 +0530

    Add files via upload

---
+++ Recursion/toh.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class toh
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n = sc.nextInt();
+
+        TOH(n, 'A', 'B', 'C');
+    }
+
+    public static void TOH(int n, char A, char B, char C)
+    {
+        if(n==1)
+        {
+            System.out.println("Move 1 from "+A+" to "+C);
+            return;
+        }
+        TOH(n-1, A, C, B);
+        System.out.println("Move "+n+" from "+A+" to "+C);
+        TOH(n-1, B, A ,C);
+    }
+}
